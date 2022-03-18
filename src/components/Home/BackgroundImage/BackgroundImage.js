d1e85795f59aa3d987c8e1fb42dba3eaf58c50c5
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-18 00:52:32 -0400

    adding more images to home page

--- src/components/Home/BackgroundImage/BackgroundImage.js
+++ src/components/Home/BackgroundImage/BackgroundImage.js
@@ -8,10 +8,32 @@ const Backgroundimage = () => {
           <Image
             src={process.env.PUBLIC_URL + "/images/background3.jpg"}
             responsive
-            style={{ height: "200", width: "50%" }}
+            style={{ height: "200", width: "50%", borderRadius: 10 }}
           ></Image>
           <Carousel.Caption>
             <h3>Elden Ring</h3>
+            <Button>Check it Out.</Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            src={process.env.PUBLIC_URL + "/images/ghostwire.jpg"}
+            responsive
+            style={{ height: "200", width: "50%", borderRadius: 10 }}
+          ></Image>
+          <Carousel.Caption>
+            <h3>Ghostwire Tokyo</h3>
+            <Button>Check it Out.</Button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Image
+            src={process.env.PUBLIC_URL + "/images/kirby.jpg"}
+            responsive
+            style={{ height: "200", width: "50%", borderRadius: 10 }}
+          ></Image>
+          <Carousel.Caption>
+            <h3>Kirby and the Forgotten Land</h3>
             <Button>Check it Out.</Button>
           </Carousel.Caption>
         </Carousel.Item>
