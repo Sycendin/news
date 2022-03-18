3a31c264aab9a774e39e3f283bc37db7ed1de6b6
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-18 02:40:58 -0400

    starting on adding top stories segment

---
+++ src/components/Home/TopStories/TopStories.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from "react";
+
+const TopStories = () => {
+  return (
+    <Fragment>
+      <div
+        style={{
+          height: 100,
+          width: "100%",
+          backgroundColor: " rgba(0, 0, 0, 0.2)",
+          marginTop: 50,
+        }}
+      >
+        <h1>Today's Top stories</h1>
+      </div>
+    </Fragment>
+  );
+};
+export default TopStories;
