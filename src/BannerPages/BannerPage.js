78cb67ac77c06abad0b45fc953e457e342cc95a3
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-03-24 00:49:10 -0400

    fixed bug where copy pasted urls don't load correct content for banner and added 404 for wrong banner url

--- src/BannerPages/BannerPage.js
+++ src/BannerPages/BannerPage.js
@@ -1,85 +1,102 @@
 import React, { Fragment } from "react";
 import Image from "react-bootstrap/Image";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import bannerData from "./BannerPageData/BannerData";
 import TextList from "../components/TextList/TextList";
+import paths from "../components/PathNames/PathNames";
+import { changeBanner } from "../actions/actions";
+import NotFound from "../components/NotFound/NotFound";
 const BannerPage = () => {
+  let error = false;
+  const dispatch = useDispatch();
+  const stringPathName = window.location.pathname;
+  let obj = paths.find((o) => o.path === stringPathName);
+  if (obj !== undefined) {
+    dispatch(changeBanner(obj.content));
+  } else {
+    error = true;
+  }
+  // dispatch(changeBanner(obj.content));
   const select = useSelector((state) => state.bannerChange);
   return (
     <Fragment>
-      <div
-        style={{
-          margin: "auto",
-          width: "66%",
-          backgroundColor: " rgb(212, 214, 215)",
-          borderRadius: 10,
-        }}
-      >
-        <h1
-          style={{
-            textDecoration: "underline",
-            textAlign: "left",
-            marginBottom: 30,
-            fontWeight: "bold",
-            marginLeft: 10,
-          }}
-        >
-          {bannerData[select].title}
-        </h1>
-        <h3
-          style={{
-            textAlign: "left",
-            marginBottom: 30,
-            marginLeft: 10,
-          }}
-        >
-          {bannerData[select].subtitle}
-        </h3>
-        <img
-          src={`${process.env.PUBLIC_URL + bannerData[select].image}`}
-          style={{
-            width: "100%",
-            marginLeft: "auto",
-            marginRight: "auto",
-            height: 400,
-          }}
-        ></img>
-
+      {error === false ? (
         <div
-          className="block-example border-bottom border-dark"
           style={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "left",
-            alignContent: "left",
-            marginTop: 10,
-            marginBottom: 5,
-            borderBottom: "3px solid rgb(212, 212, 212)",
+            margin: "auto",
+            width: "66%",
+            backgroundColor: " rgb(212, 214, 215)",
+            borderRadius: 10,
           }}
         >
-          <Image
-            src={
-              "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg"
-            }
-            roundedCircle={true}
+          <h1
             style={{
-              width: 50,
-              height: 50,
-              marginLeft: 5,
+              textDecoration: "underline",
+              textAlign: "left",
+              marginBottom: 30,
+              fontWeight: "bold",
+              marginLeft: 10,
             }}
-          ></Image>
-          <p style={{ marginTop: 10, marginLeft: 5, fontWeight: "bold" }}>
-            By John Doe
-          </p>
-          <p style={{ marginTop: 10, marginLeft: 5 }}>
-            | Posted: March 22, 2022
-          </p>
+          >
+            {bannerData[select].title}
+          </h1>
+          <h3
+            style={{
+              textAlign: "left",
+              marginBottom: 30,
+              marginLeft: 10,
+            }}
+          >
+            {bannerData[select].subtitle}
+          </h3>
+          <img
+            src={`${process.env.PUBLIC_URL + bannerData[select].image}`}
+            style={{
+              width: "100%",
+              marginLeft: "auto",
+              marginRight: "auto",
+              height: 400,
+            }}
+          ></img>
+
+          <div
+            className="block-example border-bottom border-dark"
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "left",
+              alignContent: "left",
+              marginTop: 10,
+              marginBottom: 5,
+              borderBottom: "3px solid rgb(212, 212, 212)",
+            }}
+          >
+            <Image
+              src={
+                "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg"
+              }
+              roundedCircle={true}
+              style={{
+                width: 50,
+                height: 50,
+                marginLeft: 5,
+              }}
+            ></Image>
+            <p style={{ marginTop: 10, marginLeft: 5, fontWeight: "bold" }}>
+              By John Doe
+            </p>
+            <p style={{ marginTop: 10, marginLeft: 5 }}>
+              | Posted: March 22, 2022
+            </p>
+          </div>
+          <div>
+            <TextList data={bannerData[select].text} />
+          </div>
+          {/* <h3 style={{ whiteSpace: "pre-line" }}>{bannerData[select].text}</h3> */}
         </div>
-        <div>
-          <TextList data={bannerData[select].text} />
-        </div>
-        {/* <h3 style={{ whiteSpace: "pre-line" }}>{bannerData[select].text}</h3> */}
-      </div>
+      ) : (
+        <NotFound />
+      )}
     </Fragment>
   );
 };
---
+++ src/components/PathNames/PathNames.js
@@ -0,0 +1,19 @@
+const paths = [
+  {
+    path: "/banner/elden",
+    content: 0,
+  },
+  {
+    path: "/banner/ghostwire",
+    content: 1,
+  },
+  {
+    path: "/banner/kirby",
+    content: 2,
+  },
+  {
+    title: "Top 10 Indie Games You Should Play",
+    image: "/images/topstories/indie.jpg",
+  },
+];
+export default paths;
